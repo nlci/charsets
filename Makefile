7e00be43e559d8d6be62faafc05985f778060da9
Bobby de Vos <bobby_devos@sil.org>
2018-03-16 13:06:00 -0600

    Several updates
    - NLCI Kannada character set for pyfontaine
    - Provide character names for Tamil
    - NLCI specific Indic and Latin character set for pyfontaine
    - Latin characters not in the NRSI Latin character list
    
    Work done by 10 or 13 Jan 2018

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 SHELL   = /bin/sh
 
-charsets=nlci-deva.py nlci-taml.py nrsi-all.py
+charsets=nlci-deva.py nlci-knda.py nlci-taml.py nlci-all.py nrsi-all.py
 
 all:
 	install -m 644 $(charsets) "$(HOME)/builds/pyfontaine/pyfontaine/fontaine/charsets/internals"
---
+++ nlci-all.py
@@ -0,0 +1,62 @@
+# -*- coding: utf8 -*-
+
+
+class Charset:
+    common_name = 'NLCI: All fonts'
+    native_name = 'Characters for all Indic fonts'
+    abbreviation = 'Indic'
+    key = 0x00E9
+    glyphs = [
+        # Indic
+        0x20A8,  # RUPEE SIGN
+        0x20B9,  # INDIAN RUPEE SIGN
+        0x0964,  # DEVANAGARI DANDA
+        0x0965,  # DEVANAGARI DOUBLE DANDA
+        # 0x0971,  # DEVANAGARI SIGN HIGH SPACING DOT
+        0x0951,  # DEVANAGARI STRESS SIGN UDATTA
+        0x0952,  # DEVANAGARI STRESS SIGN ANUDATTA
+        0x0953,  # DEVANAGARI GRAVE ACCENT
+        0x0954,  # DEVANAGARI ACUTE ACCENT
+        0x1CDA,  # VEDIC TONE DOUBLE SVARITA
+
+        # Quotations and text marking
+        0x2308,  # LEFT CEILING
+        0x2309,  # RIGHT CEILING
+        0x230A,  # LEFT FLOOR - request from Dan
+        0x230B,  # RIGHT FLOOR - request from Dan
+        0x231C,  # TOP LEFT CORNER
+        0x231D,  # TOP RIGHT CORNER
+        0x231E,  # BOTTOM LEFT CORNER - used by Steve
+        0x231F,  # BOTTOM RIGHT CORNER - used by Steve
+        # 0x2E22,  # TOP LEFT HALF BRACKET - not in Charis
+        # 0x2E23,  # TOP RIGHT HALF BRACKET - not in Charis
+        # 0x2E24,  # BOTTOM LEFT HALF BRACKET - request from Steve - not in Charis
+        # 0x2E25,  # BOTTOM RIGHT HALF BRACKET - request from Steve - not in Charis
+
+        # Footnote callers
+        # 0x2756,  # BLACK DIAMOND MINUS WHITE X - suggested by Dan - not in Charis
+
+        # Glottal
+        0x02BC,  # MODIFIER LETTER APOSTROPHE
+        0x02C0,  # MODIFIER LETTER GLOTTAL STOP
+        # 0xA78B,  # LATIN CAPITAL LETTER SALTILLO
+        0xA78C,  # LATIN SMALL LETTER SALTILLO
+        # 0x0241,  # LATIN CAPITAL LETTER GLOTTAL STOP - maybe only for Latin orthographies
+        # 0x0242,  # LATIN SMALL LETTER GLOTTAL STOP - maybe only for Latin orthographies
+
+        # Misc
+        0x2423 # OPEN BOX
+        
+        # MacRoman not in Codepage 1252
+        0x03A9,  # GREEK CAPITAL LETTER OMEGA
+
+        # Variation Selectors
+        # list(xrange(0xE0100, 0xE01EF)) # VARIATION SELECTOR-17..VARIATION SELECTOR-256 - non BMP characters not handled
+
+        # Superscripted footnote callers
+        0x2070,  # SUPERSCRIPT ZERO
+        0x207B,  # SUPERSCRIPT MINUS
+        0x207D,  # SUPERSCRIPT LEFT PARENTHESIS
+        0x207E,  # SUPERSCRIPT RIGHT PARENTHESIS
+    ] + \
+        list(xrange(0x2074, 0x2079)), # SUPERSCRIPT FOUR..SUPERSCRIPT NINEgit
