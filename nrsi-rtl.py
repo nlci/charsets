1f507b6aa88ec57048caf09778093bf0880141d7
Bobby de Vos <bobby_devos@sil.org>
2019-04-18 13:47:52 -0600

    Some characters are only needed in RTL fonts

---
+++ nrsi-rtl.py
@@ -0,0 +1,17 @@
+# -*- coding: utf8 -*-
+
+# Copyright 2018-2019 NLCI (http://www.nlci.in/fonts/)
+# Creative Commons Attribution-ShareAlike 3.0 Unported (CC BY-SA 3.0) license
+
+class Charset:
+    common_name = 'NRSI-RTL fonts'
+    native_name = 'Recommended characters for RTL fonts'
+    abbreviation = 'LatnRTL'
+    key = 0x00E9
+    glyphs = [
+        0x202A,
+        0x202B,
+        0x202C,
+        0x202D,
+        0x202E,
+    ]
